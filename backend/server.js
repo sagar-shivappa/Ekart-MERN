aaaf89d5466c8b7c18e5afca362d821af1e9c2de
Sagar R Shivappa <sagar.rs@globant.com>
2024-07-23 22:33:36 +0530

    Solution backend Done

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8002;
 const { intializeDB } = require("./utils/intializeDb");
 
 require("dotenv").config();
 
 /* Connecting to the database and then starting the server. */
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI + process.env.DB_NAME)
   .then(() => {
     intializeDB();
-    app.listen(PORT, console.log(`Server started on port http://localhost:${PORT}`));
+    app.listen(PORT, console.log(`Server started on port ${PORT}`));
   })
   .catch((err) => {
     console.log(err);
